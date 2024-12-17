923652e2dc57068e4362c9c5ca2173080a20b51e
tyPhoon <typhoonmoon1022@gmail.com>
2024-12-18 01:50:05 +0900

    Add:discordに対して、エラー時のログを追加

--- src/module/callback/discord.py
+++ src/module/callback/discord.py
@@ -6,10 +6,15 @@ class DiscordCallback(Callback):
     def __init__(self, webhook_url: str) -> None:
         self.webhook_url = webhook_url
 
+    def _sendMessage(self, message: str) -> None:
+        requests.post(self.webhook_url, json={"content": message})
+
     def on_train_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         super().on_train_end(trainer, pl_module)
 
         self._sendMessage("Training finished")
 
-    def _sendMessage(self, message: str) -> None:
-        requests.post(self.webhook_url, json={"content": message})
+    def on_exception(
+        self, trainer: Trainer, pl_module: LightningModule, exception: BaseException
+    ) -> None:
+        self._sendMessage(f"Exception occurred: {exception}")
