235c817df2196abe9e20789df5e1bd6fb9b98d4b
tyPhoon <typhoonmoon1022@gmail.com>
2024-12-22 07:59:40 +0900

    Fix:DiscordCallbackの修正

--- src/module/callback/discord.py
+++ src/module/callback/discord.py
@@ -6,15 +6,13 @@ class DiscordCallback(Callback):
     def __init__(self, webhook_url: str) -> None:
         self.webhook_url = webhook_url
 
-    def _sendMessage(self, message: str) -> None:
+    def _send_message(self, message: str) -> None:
         requests.post(self.webhook_url, json={"content": message})
 
     def on_train_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
-        super().on_train_end(trainer, pl_module)
-
-        self._sendMessage("Training finished")
+        self._send_message("Training finished")
 
     def on_exception(
         self, trainer: Trainer, pl_module: LightningModule, exception: BaseException
     ) -> None:
-        self._sendMessage(f"Exception occurred: {exception}")
+        self._send_message(f"Exception occurred: {exception}")
