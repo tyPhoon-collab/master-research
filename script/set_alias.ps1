a473f6a19885925f27199d78f5e0d7ff3fb68325
tyPhoon <typhoonmoon1022@gmail.com>
2025-01-04 09:57:19 +0900

    Add:便利スクリプトの追加

---
+++ script/set_alias.ps1
@@ -0,0 +1,11 @@
+function tu {
+    uv run main.py mode=train_unet @args
+}
+
+function tdw {
+    uv run main.py mode=train_diffwave @args
+}
+
+function doctor {
+    uv run main.py mode=doctor @args
+}
---
+++ script/set_alias.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function tu() {
+    uv run main.py mode=train_unet "$@"
+}
+
+function tdw() {
+    uv run main.py mode=train_diffwave "$@"
+}
+
+function doctor() {
+    uv run main.py mode=doctor "$@"
+}
