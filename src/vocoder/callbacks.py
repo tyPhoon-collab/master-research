59cb1608134269714bb7c4ce8a8b924cccc9f8ba
tyPhoon <typhoonmoon1022@gmail.com>
2025-02-10 10:04:45 +0900

    Fix:DiffWaveNeptuneLoggerCallbackの修正

--- src/vocoder/callbacks.py
+++ src/vocoder/callbacks.py
@@ -1,59 +1,42 @@
-import glob
+from typing import Any
 
-import torchaudio
 from lightning import LightningModule, Trainer
 
 from callback.neptune import NeptuneLoggerCallback
-from tool.config import MelConfig
-from tool.pipeline import MelSpectrogramPipeline, WaveformPipeline
 from tool.plot import fig_to_pil_image, plot_multiple, plot_spectrogram, plot_waveform
 from vocoder.module.diffwave import DiffWaveLightning
 
 
 class DiffWaveNeptuneLoggerCallback(NeptuneLoggerCallback):
-    def __init__(self, test_dir: str, hop_length: int):
+    def __init__(self, hop_length: int) -> None:
         super().__init__()
-
-        self.test_dir = test_dir
         self.hop_length = hop_length
 
-    def setup(self, trainer: Trainer, pl_module: LightningModule, stage: str) -> None:
-        super().setup(trainer, pl_module, stage)
-
-        c = MelConfig(
-            hop_length=self.hop_length,
-        )
-
-        self.pipe_mel = MelSpectrogramPipeline(c).to(pl_module.device)
-        self.pipe_waveform = WaveformPipeline(c).to(pl_module.device)
-
-    def on_train_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
-        super().on_train_epoch_end(trainer, pl_module)
-
+    def on_validation_batch_start(
+        self,
+        trainer: Trainer,
+        pl_module: LightningModule,
+        batch: Any,
+        batch_idx: int,
+        dataloader_idx: int = 0,
+    ) -> None:
         model: DiffWaveLightning = pl_module  # type: ignore
+        self._append(model, batch)
 
-        for path in glob.glob(f"{self.test_dir}/*.mp3"):
-            self._append(model, path)
-
-    def _append(self, model: DiffWaveLightning, path: str):
-        y, sr = torchaudio.load(path)
-        y = y.to(model.device)
-
-        waveform = self.pipe_waveform(y)
-        mel = self.pipe_mel(y)
+    def _append(self, model: DiffWaveLightning, batch: Any) -> None:
+        waveform = batch["waveform"]
+        mel = batch["mel"]
 
         waveform_hat = model.generate(
             mel,
             hop_length=self.hop_length,
         )
-        mel_hat = self.pipe_mel(waveform_hat)
 
         fig = plot_multiple(
             [
                 plot_waveform(waveform.squeeze().cpu().numpy()),
                 plot_waveform(waveform_hat.squeeze().cpu().numpy()),
                 plot_spectrogram(mel.squeeze().cpu().numpy()),
-                plot_spectrogram(mel_hat.squeeze().cpu().numpy()),
             ]
         )
         img = fig_to_pil_image(fig)
